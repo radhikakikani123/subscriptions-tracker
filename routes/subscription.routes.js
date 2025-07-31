51351e8621267107955716d1d3a00987b4270813
radhika kikani <radhikikani123@gmail.com>
2025-07-31 12:40:52 +0530

    Add subscription creation controller and route
    Introduces createSubscription controller for handling subscription creation. Updates POST /subscriptions route to use authorization middleware and the new controller.

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import createSubscription from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
 subscriptionRouter.get('/', (req, res) => res.send({ title: 'GET all subscriptions' }));
 subscriptionRouter.get('/:id', (req, res) => res.send({ title: 'GET subscription details' }));
-subscriptionRouter.post('/', (req, res) => res.send({ title: 'CREAT subscription' }));
+subscriptionRouter.post('/', authorize, createSubscription);
 subscriptionRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE subscription' }));
 subscriptionRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE subscription' }));
 subscriptionRouter.get('/user/:id', (req, res) => res.send({ title: 'GET all user subscriptions' }));
