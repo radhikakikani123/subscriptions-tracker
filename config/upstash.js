89c65246ccb483d78c35dae6105546263d1a9d7b
radhika kikani <radhikikani123@gmail.com>
2025-08-01 16:34:20 +0530

    Integrate Upstash workflow for subscription reminders
    Added QStash environment variables and refactored Upstash workflow client import. The subscription controller now triggers a workflow for subscription reminders with relevant data, and the workflow routes include a new endpoint to handle reminder requests.

--- config/upstash.js
+++ config/upstash.js
@@ -1,7 +1,7 @@
-import { Client as workflowClient } from "@upstash/workflow";  
+import { Client as WorkflowClient } from "@upstash/workflow";  
 import {QSTASH_URL, QSTASH_TOKEN} from './env.js';
 
-export const workflowClient = new workflowClient({
+export const workflowClient = new WorkflowClient({
     baseUrl: QSTASH_URL,
     token: QSTASH_TOKEN,
 });
--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,6 +1,6 @@
 import Subscription from '../models/subscription.model.js';
-import { workflowClient } from '@upstash/workflow';   
-import            
+import { workflowClient } from "../config/upstash.js";   
+import { SERVER_URL } from '../config/env.js';      
 
 export const createSubscription = async (req, res, next) => {
     try{
@@ -9,10 +9,19 @@ export const createSubscription = async (req, res, next) => {
         user: req.user._id,
      });
 
-     await workflowClient.trigger({
-        url: `${SERVER_URL}`
+     const { workflowRunId } = await workflowClient.trigger( {
+        url: `${SERVER_URL}/api/v1/workflows/subscriptions/reminder`,
+        body: {
+            subscriptionId: subscription.id,
+        },
+        headers: {
+            'content-type': 'application/json',
+        },
+        retries: 0,
+
      })
-     res.status(201).json({success:true, data: subscription});
+
+     res.status(201).json({success:true, data: subscription, workflowRunId});
     }
     catch(e) {
         next(e);
